4c22d574dd929c46e6686107102b02adee5467fc
Lucienne85 <66264188+Lucienne85@users.noreply.github.com>
2020-10-03 11:06:24 +0200

    First try at React including Axios

---
+++ src/Weather.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+  let [temperature, setTemperature] = useState(20);
+  let [cityName, setCityName] = useState("");
+
+  function showWeather(response) {
+    setTemperature(Math.round(response.data.main.temp));
+    setCityName(response.data.name);
+    console.log(response.data);
+  }
+
+  let apiKey = "1192a0652f0754927fef474420498ea7";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showWeather);
+
+  return (
+    <div className="weather">
+      The temperature in {cityName} is {temperature}°C
+    </div>
+  );
+}

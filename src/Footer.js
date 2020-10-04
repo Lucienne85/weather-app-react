c037c696963f992bbd955c4a7ac779adbe7eb7d9
Lucienne85 <66264188+Lucienne85@users.noreply.github.com>
2020-10-04 18:15:26 +0200

    Fixed urls to publish on Netlify

--- src/Footer.js
+++ src/Footer.js
@@ -9,11 +9,16 @@ export default function Footer() {
           href="https://github.com/Lucienne85/weather-app-react"
           target="_blank"
           className="gitLink"
+          rel="noopener noreferrer"
         >
           Open-source code
         </a>
         by Lucienne de Boer
-        <a href="https://www.linkedin.com/in/luciennedeboer/" target="_blank">
+        <a
+          href="https://www.linkedin.com/in/luciennedeboer/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img
             src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/001/973/original/linkedin.png?1597418766"
             alt="LinkedIn logo"
@@ -23,26 +28,48 @@ export default function Footer() {
       </p>
       <footer>
         Icons made by
-        <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+        <a
+          href="https://www.flaticon.com/authors/freepik"
+          title="Freepik"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Freepik
         </a>
         ,
-        <a href="https://www.flaticon.com/authors/iconixar" title="iconixar">
+        <a
+          href="https://www.flaticon.com/authors/iconixar"
+          title="iconixar"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           iconixar
         </a>
         ,
-        <a href="https://www.flaticon.com/authors/srip" title="srip">
+        <a
+          href="https://www.flaticon.com/authors/srip"
+          title="srip"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           srip
         </a>
         and
         <a
           href="https://www.flaticon.com/free-icon/snowflake_2942944"
           title="surang"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           surang
         </a>
         from
-        <a href="https://www.flaticon.com/" title="Flaticon">
+        <a
+          href="https://www.flaticon.com/"
+          title="Flaticon"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           www.flaticon.com
         </a>
       </footer>

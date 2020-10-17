5409cc759cef8652cba49d6c028645ec4ee140d6
Lucienne85 <66264188+Lucienne85@users.noreply.github.com>
2020-10-17 22:20:06 +0200

    Made the searchbar work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import Searchbar from "./Searchbar";
 import Weatherblock from "./Weatherblock";
 import Forecastblock from "./Forecastblock";
 import Footer from "./Footer";
@@ -10,7 +9,6 @@ export default function App() {
     <div className="App">
       <div className="container">
         <h1 className="Header">Weather App</h1>
-        <Searchbar />
         <Weatherblock defaultCity="Amsterdam" />
         <Forecastblock />
         <Footer />
